edd1cc659a2e4331064e4317d8ffb87c58deb00f
TheoryOfNekomata <allan.crisostomo@outlook.com>
2025-09-27 15:50:04 +0800

    Initial commit
    Add files from create-react-router and create-fastify

---
+++ packages/backend/routes/root.ts
@@ -0,0 +1,7 @@
+import {FastifyInstance} from 'fastify';
+
+export default async function (fastify: FastifyInstance, opts: unknown) {
+  fastify.get('/', async function (request, reply) {
+    return { root: true }
+  })
+}
